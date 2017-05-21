3f3d2b90839464d3cdd8617160cace52fbca8af2
zurus93 <zurus93@gmail.com>
2017-05-21 18:37:51 +0200

    First implementation - cars going in straight line and stopping at the gates.

--- ExpresswaySimulation/src/expresswaysimulation/agents/Gate.java
+++ ExpresswaySimulation/src/expresswaysimulation/agents/Gate.java
@@ -1,5 +1,21 @@
 package expresswaysimulation.agents;
 
-public class Gate {
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.grid.Grid;
 
+/**
+ * Class representing gate at the expressway.
+ * It is immobile and is the place where cars make payment for crossing.
+ * @author FUKSIK
+ *
+ */
+public class Gate {
+	
+	private ContinuousSpace<Object> mSpace;
+	private Grid<Object> mGrid;
+	
+	public Gate(ContinuousSpace<Object> space, Grid<Object> grid) {
+		mSpace = space;
+		mGrid = grid;
+	}
 }
