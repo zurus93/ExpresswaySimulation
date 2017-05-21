b8b2d61db7d6c35b3a28964778b30a52fefd241a
zurus93 <zurus93@gmail.com>
2017-05-21 18:39:37 +0200

    First implementation - cars going in straight line and stopping at the gates.

---
+++ ExpresswaySimulation/src/expresswaysimulation/util/Params.java
@@ -0,0 +1,11 @@
+package expresswaysimulation.util;
+
+public class Params {
+    
+    public final static int GRID_WIDTH = 50;
+    
+    public final static int GRID_HEIGHT = 50;
+    
+    public final static int NUMBER_OF_CARS = 20;
+
+}
